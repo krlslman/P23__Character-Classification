09d6dd7fbb307aedd34e0d8b0958871cdfbae195
krlslman <selmankorall@gmail.com>
2022-06-22 23:39:08 +0300

    counters defined in inner scope

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 // const input = document.querySelector(".input-text").valuelet inputValue = 'initial' ;
-let setOfChars = new Array;
+
 // setOfChars = ['i'];
-let inputToArray; //! for space counting and most frequent
+
 let vowelsArray = "aeiouüıö".split("");
 let consonantsArray = "qwrtypğsdfghjklşzxcvbnmç".split("");
 let othersArray = "!'^+%&/()=?_>£#$½{[]}|.,:;`~¨|<>@æß€".split("");
 
-let counterVowels = 0;
-let counterConsonant = 0;
-let counterOthers = 0;
-let counterWords =  0;
+
 
 
 let button = document.querySelector('.input-btn');
@@ -20,11 +17,27 @@ let newEntryLi = document.createElement('li');
 
 // const getValueInput = () => {
 button.addEventListener('click', () => {
+
+	let counterVowels = 0;
+	let counterConsonant = 0;
+	let counterOthers = 0;
+	let counterWords =  0;
+	let setOfChars = new Array;
+	let inputToArray; //! for space counting and most frequent
+
+
 	inputValue = document.querySelector(".input-text").value; 
 	inputToArray = inputValue.split(""); 
 
 	// assign each input char to the set and inrease related counter
 	for (let i = 0 ; i<inputValue.length ; i++) {
+		if (inputValue==''){
+			// counterWords = 0;
+			// counterVowels = 0;
+			// counterConsonant = 0;
+			// counterOthers = 0;
+
+		}
 		if (!setOfChars.includes(inputValue[i]) && consonantsArray.includes(inputValue[i])) {
 			setOfChars.push(inputValue[i]);
 			counterConsonant++;
@@ -44,7 +57,7 @@ button.addEventListener('click', () => {
 		}
 	};
 
-	console.log('Sesli harf adeti : ' + counterVowels + '\nSessiz harf adeti : ' + counterConsonant + '\nNoktalama işareti ve sembol adeti : ' + counterOthers );
+	// console.log('Sesli harf adeti : ' + counterVowels + '\nSessiz harf adeti : ' + counterConsonant + '\nNoktalama işareti ve sembol adeti : ' + counterOthers );
 	
 	
 	// newEntryLi.appendChild( "fdhdf");
@@ -55,6 +68,12 @@ button.addEventListener('click', () => {
 	<li class="result-li">Number of consonants  <span class="colon"> : </span> <span class="result-value">${counterConsonant}</span></li>
 	<li class="result-li">Number of symbols 	<span class="colon"> : </span> <span class="result-value">${counterOthers}</span></li>
 	`;
+
+
+	// counterVowels = 0;
+	// counterConsonant = 0;
+	// counterOthers = 0;
+	// counterWords =  0;
 });
 
 //! Calling click function of add button by pressing enter key from the keyboard
